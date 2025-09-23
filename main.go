79a396148b30e8c04bf6e07803037de4de0f5d5e
suziheng <suziheng@forchange.tech>
2025-09-23 16:24:33 +0800

    feat: update version

--- main.go
+++ main.go
@@ -150,7 +150,7 @@ func main() {
 	if port == "" {
 		port = strconv.Itoa(*common.Port)
 	}
-	log.Println("v1.0.1 Listening and serving HTTP on :" + port)
+	log.Println("v1.0.3 Listening and serving HTTP on :" + port)
 	err = server.Run(":" + port)
 	if err != nil {
 		common.FatalLog("failed to start HTTP server: " + err.Error())
